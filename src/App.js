8ccc82db6c954d0ad51b5d27402779b4f5e75b93
MiguelGoda <miguel_g14@hotmail.com>
2021-04-13 18:15:52 -0400

    agregando en el trabajo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Card from './components/Card'
+
 function App() {
   return(
     <div>
-      <h1>hello world</h1>
+      <Card/>
     </div>
   )
 }
---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className='card'>
+            <div className='card-body'>
+                <h4 className='card-tittle'>My Tittle</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatum sapiente tenetur recusandae aperiam quasi officia iste esse porro, illum aspernatur, quod et consequatur. Id beatae repellendus ullam ipsum culpa?</p>
+            </div>
+            card
+        </div>
+    )
+}
+
+export default Card
