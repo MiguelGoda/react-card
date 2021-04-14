076d727b6ac9b3d03733a3caf34545fedf49a848
Desputicus <miguel_g14@hotmail.com>
2021-04-14 00:23:50 -0400

    agregado de imagenes

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,8 @@ function Card() {
         <div className='card'>
             <div className='card-body'>
                 <h4 className='card-tittle'>My Tittle</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatum sapiente tenetur recusandae aperiam quasi officia iste esse porro, illum aspernatur, quod et consequatur. Id beatae repellendus ullam ipsum culpa?</p>
+                <p className="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatum sapiente tenetur recusandae aperiam quasi officia iste esse porro, illum aspernatur, quod et consequatur. Id beatae repellendus ullam ipsum culpa?</p>
             </div>
-            card
         </div>
     )
 }
