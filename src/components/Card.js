97e4396e7762f4006480e439bf594a3cae90bb21
Desputicus <miguel_g14@hotmail.com>
2021-04-14 23:50:21 -0400

    agregado en casa mas limintaciones

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-
-
-function Card() {
-    return (
-        <div className='card text-center bg-dark'>
-            <img src="" alt="imagen1" />
-            <div className='card-body text-light'>
-                <h4 className='card-tittle'>My Tittle</h4>
-                <p className="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatum sapiente tenetur recusandae aperiam quasi officia iste esse porro, illum aspernatur, quod et consequatur. Id beatae repellendus ullam ipsum culpa?</p>
-                <a href="#!" className="btn btn-outline-secondary rounded-0">
-                    Go to this website
-                </a>
-            </div>
-        </div>
-    )
+function Card({ title, imageSource, text, imagealt, url }) {
+  return (
+    <div className="card text-center bg-dark">
+      <img src={imageSource} alt={imagealt} />
+      <div className="card-body text-light">
+        <h4 className="card-tittle">{title}</h4>
+        <p className="card-text text-secondary">
+          {text
+            ? text
+            : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatum sapiente tenetur recusandae aperiam quasi officia iste esse porro, illum aspernatur, quod et consequatur. Id beatae repellendus ullam ipsum culpa?"}
+        </p>
+        <a href={url} className="btn btn-outline-secondary rounded-0">
+          Go to this website
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string,
+  imageSource: PropTypes.string,
+  text: PropTypes.string,
+};
+
+export default Card;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,40 +1,49 @@
 import React from "react";
 import Card from "./Card";
 
-import image1 from '../assets/image1.jpg'
-import image2 from '../assets/image2.jpg'
-import image3 from '../assets/image3.jpg'
-
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
 
 const cards = [
   {
-    id:1,
-    title:'Faz Web',
-    image:image1
+    id: 1,
+    title: "Faz Web",
+    image: image1,
+    alt: "imagen de la web",
+    url: "https://faztweb.com",
   },
   {
-    id:2,
-    title:'Fazt Blog',
-    image:image2
+    id: 2,
+    title: "Fazt Blog",
+    image: image2,
+    alt: "imagen del blog",
+    url: "https://blog.faztweb.com",
   },
   {
-    id:3,
-    title:'Fazt Youtube',
-    image:image3
-  }
-]
+    id: 3,
+    title: "Fazt Youtube",
+    image: image3,
+    alt: "imagen de youtube",
+    url: "https://youtube.com",
+  },
+];
 
 function Cards() {
   return (
     <div className="container d-flex justify-content-center align-items-center h-100">
       <div className="row">
-        {
-          cards.map(card => (
-            <div className="col-md-4" key={card.id}>
-              <Card />
-            </div>
-          ))
-        }
+        {cards.map((card) => (
+          <div className="col-md-4" key={card.id}>
+            <Card
+              title={card.title}
+              imageSource={card.image}
+              imagealt={card.alt}
+              url={card.url}
+              text={card.text}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
